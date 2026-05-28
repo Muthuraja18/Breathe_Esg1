4593d1a576f38577b6bcf6efbc22cd7fba5ad2b9
Muthuraja18 <muthurajlingam788@gmail.com>
2026-05-29 00:35:36 +0530

    Add home view to URL patterns

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-
 from django.urls import path, include
 
 from django.conf import settings
-
 from django.conf.urls.static import static
+
+from .views import home
 
 
 urlpatterns = [
+
+    path('', home),
 
     path(
         'admin/',
